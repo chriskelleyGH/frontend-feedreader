5c21613931465fb2713b3d48b2f9bacf8c7f3b34
Chris Kelley <ckemail2@gmail.com>
2019-08-17 11:41:42 -0400

    add menu tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,10 +32,10 @@ $(function() {
          * and that the URL is not empty.
          */
          it('URL defined and not empty', function() {
-            for(feed of allFeeds) {
-                expect(feed.url).toBeDefined();
-                expect(feed.url.length).not.toBe(0);
-            }
+             for(feed of allFeeds) {
+                 expect(feed.url).toBeDefined();
+                 expect(feed.url.length).not.toBe(0);
+             }
          });
 
         /* TODO: Write a test that loops through each feed
@@ -43,28 +43,45 @@ $(function() {
          * and that the name is not empty.
          */
          it('name defined and not empty', function() {
-            for(feed of allFeeds) {
-                expect(feed.name).toBeDefined();
-                expect(feed.name.length).not.toBe(0);
-            }
-         });
+             for(feed of allFeeds) {
+                 expect(feed.name).toBeDefined();
+                 expect(feed.name.length).not.toBe(0);
+             }
+          });
 
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('is hidden by default', function() {
+             const body = document.querySelector('body');
+             expect(body.classList.contains('menu-hidden')).toBe(true);
+         });
+
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          it('changes when clicked', function() {
+              const body = document.querySelector('body');
+              const menu = document.querySelector('.menu-icon-link');
+
+              menu.click();
+              expect(body.classList.contains('menu-hidden')).toBe(false);
+              menu.click();
+              expect(body.classList.contains('menu-hidden')).toBe(true);
+          });
+
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
